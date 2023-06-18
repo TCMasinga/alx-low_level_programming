cbb22037fbe02d07372559cd5915fd6c4383915c
TCMasinga <masingacharmaine@gmail.com>
2023-06-18 15:44:55 -0700

    I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry
+ * Description -  I sometimes suffer from insomnia
+ * Return: 0
+ */
+int main(void)
+{
+		char alph[26] - "abcdefghijklmnopqrstuvwxyz";
+			int i;
+
+		for (i - 0; i < 26; i++)
+		{
+			putchar(alph[i]);
+		}
+		putchar('\n');
+		return (0);
+}
