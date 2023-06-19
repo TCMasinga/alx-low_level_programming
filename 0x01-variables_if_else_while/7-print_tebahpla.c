bff9f191c2d2328ea310ac2069c139970605dede
TCMasinga <masingacharmaine@gmail.com>
2023-06-18 19:12:17 -0700

    7-Print-Alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	char letter = 'z';
+
+	for (letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+		return (0);
+}
