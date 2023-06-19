c06979ccf5c462103d9ff66fd1d34c0fa170a06a
TCMasinga <masingacharmaine@gmail.com>
2023-06-18 20:41:19 -0700

    8-print_base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	char i;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
