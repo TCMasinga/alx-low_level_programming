ae1acd272c6fa3a0bf9a41d6ab4a90d9aeb99fb1
TCMasinga <masingacharmaine@gmail.com>
2023-06-26 13:50:59 -0700

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,11 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0') 
-		i++;
+	while (s[i] != '\0')
+	i++;
 	for (i = i - 1; i >= 0; i--)
 	{
-		_putchar(s[i]);
+	_putchar(s[i]);
 	}
 	_putchar('\n');
 }
