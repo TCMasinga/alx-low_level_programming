b56dca6bd5a4a49027cea0672f87f0ed0243af4e
TCMasinga <masingacharmaine@gmail.com>
2023-07-11 06:09:39 -0700

    1-strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,29 +11,24 @@
 char *_strdup(char *str)
 {
 	char *dupl;
-	int i;
-	int len;
-
-	i = 0;
-	len = 0;
+	int i = 0;
+	int len = 0;
 
 	if (str == NULL)
 		return (NULL);
-	while (str[len])
-		len++;
-	dupl = malloc(sizeof(char) * (len + 1));
+	i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	dupl = malloc(sizeof(char) * (i + 1));
 
 	if (dupl == NULL)
 		return (NULL);
 
-	while (str[i] != '\0')
-	{
-			i++;
-	}
-
 	for (len = 0; str[len]; len++)
-	{
+
 		dupl[len] = str[len];
-	}
+
 	return (dupl);
 }
