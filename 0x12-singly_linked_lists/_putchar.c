cdb91e502e737a023a9822dd417aa0f0049bf8d3
TCMasinga <masingacharmaine@gmail.com>
2023-07-27 10:56:24 -0700

    0-print_list

---
+++ 0x12-singly_linked_lists/_putchar.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <unistd.h>
+
+/**
+ * _putchar - write the character to stdout
+ * @c: the character to print
+ * Return: 1 on success, -1 on error
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
